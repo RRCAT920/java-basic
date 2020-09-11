fed97253185658e20245217b8fe2cded55818da7
胡子豪 <wuqieor@outlook.com>
2020-09-11 12:16:11 +0800

    网络编程的练习

--- lang/src/net/NetTest.java
+++ lang/src/net/NetTest.java
@@ -22,6 +22,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author huzihao
@@ -50,7 +55,7 @@ public class NetTest {
         try (var serverSocket = new ServerSocket(9999);
              var socket = serverSocket.accept();
              var in = socket.getInputStream();
-             var bytesOut = new ByteArrayOutputStream();) {
+             var bytesOut = new ByteArrayOutputStream()) {
 
             var bytes = new byte[10];
             int length;
@@ -201,9 +206,10 @@ public class NetTest {
 
     @Test
     public void sender() {
-        String data = "你好，我是UDP发送端。";
         try (var soc = new DatagramSocket()) {
-            var packet = new DatagramPacket(data.getBytes(), 0, data.length(),
+            var data = "你好，我是UDP发送端。";
+            var bytes = data.getBytes();
+            var packet = new DatagramPacket(bytes, 0, bytes.length,
                     InetAddress.getLocalHost(), 8000);
             soc.send(packet);
         } catch (IOException e) {
@@ -357,56 +363,541 @@ public class NetTest {
             e.printStackTrace();
         }
     }
+
     /**
      * 编写一个服务器和一个客户端
      * 1)服务器上保存了几个用户名和对应的密码;且能验证客户端发送过来的用户名和密码,是否和保存的某个用户名和对应的密码一致.
      * 2)客户端能连接到服务器，并把用户在键盘上输入的用户名和密码，发送到服务器上
      */
+    @Test
+    public void loginServer() {
+        var nameToPassword = new HashMap<String, String>();
+        nameToPassword.put("张三", "123");
+        nameToPassword.put("李四", "321");
+        nameToPassword.put("王五", "456");
+        nameToPassword.put("赵六", "654");
+
+        try (var serverSoc = new ServerSocket(8000);
+             var soc = serverSoc.accept();
+             var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+             var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+            var req = socRd.readLine();
+            var andIndex = req.indexOf('&');
+            var name = req.substring(0, andIndex);
+            var password = req.substring(andIndex + 1);
+
+            var rs = name + "，";
+            if (password.equals(nameToPassword.get(name))) rs += "登录成功！😄";
+            else rs += "登录失败！😔";
+            socWr.write(rs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void loginClient() {
+        System.out.println("尝试连接 127.0.0.1:8000 ...");
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+             var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+             var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+            // 由于IDEA的单元测试不支持命令行输入，故使用字符串。
+            socWr.write("张三&123");
+            socWr.flush();
+            soc.shutdownOutput();
+
+            System.out.println(socRd.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * 编写一个ATM服务器和一个客户端,可以通过客户端输入帐号,密码,然后利用服务器端验证,且能实现ATM存钱,取钱和修改密码的功能
      */
+    @Test
+    public void ATMServer() {
+        var map = new HashMap<User, Account>();
+        init(map);
+        ServerSocket serverSoc = null;
+        try {
+            serverSoc = new ServerSocket(8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (null == serverSoc) return;
+
+        while (true) {
+            try (var soc = serverSoc.accept();
+                 var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                 var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+
+                var request = socRd.readLine();
+                var user = readUser(request);
+                var result = "用户名或密码错误😔";
+
+                if (map.containsKey(user)) {
+                    var account = map.get(user);
+                    label: if (request.contains("+")) {
+                        var amount = Double.parseDouble(readValue(request, "+"));
+
+                        if (amount <= 0) {
+                            result = "不能存入非正数，存钱失败。";
+                            break label;
+                        }
+                        account.deposit(amount);
+                        result = "成功存入" + amount;
+                    } else if (request.contains("-")) {
+                        var amount = Double.parseDouble(readValue(request, "-"));
+
+                        if (amount > account.getBalance()) {
+                            result = "存款不足，取钱失败。";
+                            break label;
+                        }
+                        account.withdraw(amount);
+                        result = "成功取出" + amount;
+                    } else if (request.contains("#")){
+                        map.remove(user);
+
+                        var password = readValue(request, "#");
+                        user.setPassword(password);
+                        map.put(user, account);
+                        result = "密码修改成功。";
+                    } else result = "登录成功😄";
+                }
+
+                socWr.write(result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // 0.登录✅
+    // 1.存钱✅
+    // 2.取钱✅
+    // 3.修改密码✅
+    @Test
+    public void ATMClient() {
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+             var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+             var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+
+            socWr.write("123&456");
+            socWr.flush();
+            soc.shutdownOutput();
+
+            var result = socRd.readLine();
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readValue(String request, String pattern) {
+        var index = request.indexOf(pattern);
+        return request.substring(index + 1);
+    }
+
+    private void init(Map<User, Account> map) {
+        map.put(new User("123", "123"), new Account(1000));
+    }
+
+    private User readUser(String request) {
+        int separatorIndex = request.length();
+        if (request.contains("+")) separatorIndex = request.indexOf("+");
+        else if (request.contains("-")) separatorIndex = request.indexOf("-");
+        else if (request.contains("#")) separatorIndex = request.indexOf("#");
+
+        int andIndex = request.indexOf("&");
+        var name = request.substring(0, andIndex);
+        var password = request.substring(andIndex + 1, separatorIndex);
+        return new User(name, password);
+    }
+
+    static class User {
+        String id;
+        String password;
+
+        public User(String id, String password) {
+            this.id = id;
+            this.password = password;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(id, user.id) &&
+                    Objects.equals(password, user.password);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, password);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    static class Account {
+        double balance;
+
+        public Account(double balance) {
+            this.balance = balance;
+        }
+
+        public void deposit(double amt) {
+            balance += amt;
+        }
+
+        public void withdraw(double amt) {
+            balance -= amt;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Account account = (Account) o;
+            return Double.compare(account.balance, balance) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(balance);
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+    }
+
     /**
      * 网络编程
      * 用网络编程编写一个服务端
      * 编写一个客户端，客户端向服务端发送一条信息
      * 服务端在这条信息前面加上“服务端：”后再返给客户端输出。
      */
+    @Test
+    public void dummyServer() {
+        ServerSocket serverSoc = null;
+        try {
+            serverSoc = new ServerSocket(8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (null == serverSoc) return;
+
+        while (true) {
+            try (var soc = serverSoc.accept();
+                 var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                 var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+
+                String line = socRd.readLine();
+                socWr.write("服务器端：" + line);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void dummyClient() {
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+             var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+             var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+
+            socWr.write("我是容蓉小猫咪🐈");
+            socWr.flush();
+            soc.shutdownOutput();
+
+            String line = socRd.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 网络聊天功能（50分）
      * 用网络编程编写一个服务端（5分）
      * 用网络编写一个客户端（5分）
-     * 客户端输入 client，服务端响应 I'SERVER!（40分）
+     * 客户端输入 client，服务端响应 I'am Server!（40分）
      */
+    @Test
+    public void foolServer() {
+        ServerSocket serverSoc = null;
+        try {
+            serverSoc = new ServerSocket(8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (null == serverSoc) return;
+
+        while (true) {
+            try (var soc = serverSoc.accept();
+                 var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                 var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+                String line = socRd.readLine();
+                if ("client".equals(line)) socWr.write("I'am Server!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void foolClient() {
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+            var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+            var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+            socWr.write("hello");
+            socWr.flush();
+            soc.shutdownOutput();
+
+            String line = socRd.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 实现服务器端和客户端的单线通话（50分）
-     * 实例化一个服务器端(10分)
-     * 实例化一个客户端（10分）
+     * 实现一个服务器端(10分)
+     * 实现一个客户端（10分）
      * 服务器端和客户端相连接（10分）
-     * 一旦服务器端接收到客户端的连接，服务器端就向客户端输出“welcom”（20分）
+     * 一旦服务器端接收到客户端的连接，服务器端就向客户端输出“welcome”（20分）
      */
+    @Test
+    public void emmmServer() {
+        ServerSocket serverSoc = null;
+        try {
+            serverSoc = new ServerSocket(8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (null == serverSoc) return;
+
+        while (true) {
+            try (var incoming = serverSoc.accept();
+                 var socWr = new BufferedWriter(new OutputStreamWriter(incoming.getOutputStream()))) {
+                socWr.write("welcome");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void emmmClient() {
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+             var socRd = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+            System.out.println(socRd.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * TCP/IP通信协议和IO
-     * 实例化一个服务端（10分）
-     * 实例化一个客户端，客户端可以向服务端发送一条信息（10分）
+     * 实现一个服务端（10分）
+     * 实现一个客户端，客户端可以向服务端发送一条信息（10分）
      * 用IO向D盘下面创建temp.txt文件（10分）
      * 服务端把接收到客户端的信息写到temp.txt文件中(20分)
      */
+    @Test
+    public void tempServer() {
+        try (var serverSoc = new ServerSocket(8000);
+             var incoming = serverSoc.accept();
+             var socRd = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+             var fileWr = new BufferedWriter(new FileWriter("temp-server.txt"))) {
+            String line;
+            while (null != (line = socRd.readLine())) {
+                fileWr.write(line);
+                fileWr.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void tempClient() {
+        try (var soc = new Socket(InetAddress.getLocalHost(), 8000);
+            var fileRd = new BufferedReader(new FileReader("temp-client.txt"));
+            var socWr = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()))) {
+            String line;
+            while (null != (line = fileRd.readLine())) {
+                socWr.write(line);
+                socWr.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 利用UDP编写一个程序,通过这个程序将一个字符串传给另外一台电脑中同样的程序
      */
-    /**
-     * 利用UDP编写一个程序,通过这个程序将一个字符串传给其他多台电脑中同样的程序
-     */
-    /**
-     * 利用UDP编写一个程序,通过这个程序将键盘输入的一句话传给其他多台电脑中同样的程序
-     */
+    @Test
+    public void strSender() {
+        try (var soc = new DatagramSocket()) {
+            var str = "👋，我是通过UDP协议传输的";
+            var bytes = str.getBytes();
+            var packet = new DatagramPacket(bytes, 0, bytes.length,
+                    InetAddress.getLocalHost(), 8000);
+            soc.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void strReceiver() {
+        try (var soc = new DatagramSocket(8000)) {
+            var buffer = new byte[1 << 16]; // aka 64k
+            var packet = new DatagramPacket(buffer, 0, buffer.length);
+
+            soc.receive(packet);
+
+            var str = new String(packet.getData(), 0, packet.getLength(), StandardCharsets.UTF_8);
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 利用UDP实现一对多聊天,即在一台电脑上输入要说的话,可以在其它多台电脑上展示出来
      */
+    @Test
+    public void sendToManyHost() {
+        try (var soc = new DatagramSocket()) {
+            var data = "🆔EA的单元测试有🐛";
+            var bytes = data.getBytes();
+            var packets = new ArrayList<DatagramPacket>();
+            var destPort = 8000;
+            for (int i = 0; i < 10; i++) {
+                var packet = new DatagramPacket(bytes, 0, bytes.length,
+                        InetAddress.getLocalHost(), destPort + i);
+                packets.add(packet);
+            }
+
+            for (var packet : packets) {
+                soc.send(packet);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // fixme 🆔EA多线程bug
+    @Test
+    public void tenServer() {
+        for (int i = 0; i < 10; i++) {
+            final int[] arr = {i};
+            new Thread(() -> startupServer(8000 + arr[0])).start();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            final int[] arr = {i};
+            new Thread(() -> startupServer(8000 + arr[0])).start();
+        }
+    }
+
+    private static void startupServer(int port) {
+        try (var soc = new DatagramSocket(port)) {
+            var buffer = new byte[1 << 16]; // aka 64k
+            var packet = new DatagramPacket(buffer, 0, buffer.length);
+            soc.receive(packet);
+
+            var incoming = port + ":" + new String(packet.getData(), 0, packet.getLength());
+            System.out.println(incoming);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 利用UDP和TCP实现
      * 服务器利用UDP将自己的IP和端口号发送给客户端
      * 客户端依据服务器段返回的IP和端口号,利用TCP连接到服务器
      */
+    @Test
+    public void smartServer() {
+        try (var soc = new DatagramSocket()) {
+            var localHost = InetAddress.getLocalHost();
+            var data = localHost.getHostName() + ":" + 8000;
+            var bytes = data.getBytes();
+            var packet = new DatagramPacket(bytes, 0, bytes.length, localHost, 7000 );
+
+            soc.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (var serverSoc = new ServerSocket(8000);
+             var incoming = serverSoc.accept();
+             var socWr = incoming.getOutputStream()) {
+            socWr.write("连接🔗成功😄".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void smartClient() {
+        String addr = null;
+        try (var soc = new DatagramSocket(7000)) {
+            var buffer = new byte[1 << 16]; // 64k
+            var packet = new DatagramPacket(buffer, 0, buffer.length);
+
+            soc.receive(packet);
+            addr = new String(packet.getData(), 0, packet.getLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (null == addr) return;
+        var separatorIndex = addr.indexOf(":");
+        var ip = addr.substring(0, separatorIndex);
+        var port = Integer.parseInt(addr.substring(separatorIndex + 1));
+
+        try (var soc = new Socket(ip, port);
+             var socRd = soc.getInputStream();
+             var bytesOut = new ByteArrayOutputStream()) {
+            var buffer = new byte[1024];
+            int length;
+            while (-1 != (length = socRd.read(buffer))) {
+                bytesOut.write(buffer, 0, length);
+            }
+
+            System.out.println(bytesOut.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

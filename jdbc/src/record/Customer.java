4de1695ecb9648e3c56038c14dd699c429b14c28
胡子豪 <wuqieor@outlook.com>
2020-09-25 20:32:26 +0800

    ORM

---
+++ jdbc/src/record/Customer.java
@@ -0,0 +1,82 @@
+package record;
+
+import java.sql.Date;
+import java.util.Objects;
+
+/**
+ * @author huzihao
+ * @since 2020/9/25 12:57
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private Date birth;
+
+    public Customer() {}
+
+    public Customer(int id, String name, String email, Date birth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id &&
+                Objects.equals(name, customer.name) &&
+                Objects.equals(email, customer.email) &&
+                Objects.equals(birth, customer.birth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, birth);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birth=" + birth +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+}

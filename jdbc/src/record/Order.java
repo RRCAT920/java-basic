12992db936d9e89a43144914ff7997e6628af26f
胡子豪 <wuqieor@outlook.com>
2020-09-25 22:23:54 +0800

    order表对应的类

---
+++ jdbc/src/record/Order.java
@@ -0,0 +1,69 @@
+package record;
+
+import java.sql.Date;
+import java.util.Objects;
+
+/**
+ * @author huzihao
+ * @since 2020/9/25 21:43
+ */
+public class Order {
+    private int orderId;
+    private String orderName;
+    private Date orderDate;
+
+    public Order() {}
+    public Order(int orderId, String orderName, Date orderDate) {
+        this.orderId = orderId;
+        this.orderName = orderName;
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderId == order.orderId &&
+                Objects.equals(orderName, order.orderName) &&
+                Objects.equals(orderDate, order.orderDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, orderName, orderDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderName='" + orderName + '\'' +
+                ", orderDate=" + orderDate +
+                '}';
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+}

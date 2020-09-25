b65505979150e5b8c5cd5ef1c0aa186114017fdf
胡子豪 <wuqieor@outlook.com>
2020-09-25 20:32:35 +0800

    String工具类

---
+++ jdbc/src/util/StringUtils.java
@@ -0,0 +1,98 @@
+package util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author huzihao
+ * @since 2020/9/25 17:33
+ */
+public interface StringUtils {
+    static Set<String> getKeywords() {
+        var hashSet = new HashSet<String>();
+//        abstract   continue   for          new         switch
+        {
+            hashSet.add("abstract");
+            hashSet.add("continue");
+            hashSet.add("for");
+            hashSet.add("new");
+            hashSet.add("switch");
+        }
+//        assert     default    if           package     synchronized
+        {
+            hashSet.add("assert");
+            hashSet.add("default");
+            hashSet.add("if");
+            hashSet.add("package");
+            hashSet.add("synchronized");
+        }
+//        boolean    do         goto         private     this
+        {
+            hashSet.add("boolean");
+            hashSet.add("do");
+            hashSet.add("goto");
+            hashSet.add("private");
+            hashSet.add("this");
+        }
+//        break      double     implements   protected   throw
+        {
+            hashSet.add("break");
+            hashSet.add("double");
+            hashSet.add("implements");
+            hashSet.add("protected");
+            hashSet.add("throw");
+        }
+//        byte       else       import       public      throws
+        {
+            hashSet.add("byte");
+            hashSet.add("else");
+            hashSet.add("import");
+            hashSet.add("public");
+            hashSet.add("throws");
+        }
+//        case       enum       instanceof   return      transient
+        {
+            hashSet.add("case");
+            hashSet.add("enum");
+            hashSet.add("instanceof");
+            hashSet.add("return");
+            hashSet.add("transient");
+        }
+//        catch      extends    int          short       try
+        {
+            hashSet.add("catch");
+            hashSet.add("extends");
+            hashSet.add("int");
+            hashSet.add("short");
+            hashSet.add("try");
+        }
+//        char       final      interface    static      void
+        {
+            hashSet.add("char");
+            hashSet.add("final");
+            hashSet.add("interface");
+            hashSet.add("static");
+            hashSet.add("void");
+        }
+//        class      finally    long         strictfp    volatile
+        {
+            hashSet.add("class");
+            hashSet.add("finally");
+            hashSet.add("long");
+            hashSet.add("strictfp");
+            hashSet.add("volatile");
+        }
+//        const      float      native       super       while
+//        _ (underscore)
+        {
+            hashSet.add("const");
+            hashSet.add("float");
+            hashSet.add("native");
+            hashSet.add("super");
+            hashSet.add("while");
+            hashSet.add("_");
+        }
+
+        return hashSet;
+    }
+}

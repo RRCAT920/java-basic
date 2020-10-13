1c9f0543c9c120a71470bd2918aac1fce3f2ab85
胡子豪 <wuqieor@outlook.com>
2020-10-13 21:13:25 +0800

    使用数据库连接池中的连接

--- jdbc/src/pool/C3P0Test.java
+++ jdbc/src/pool/C3P0Test.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.Test;
 import java.beans.PropertyVetoException;
 import java.sql.SQLException;
 
+import dao_opt.impl.CustomerDAOImpl;
+
 
 /**
  * @author huzihao
  * @since 2020/10/13 20:09
  */
 public class C3P0Test {
+
+    private static final ComboPooledDataSource
+            DATA_SOURCE = new ComboPooledDataSource("c3p0App");
 
     @Test
     public void connectWithHardCode() throws PropertyVetoException, SQLException {
@@ -32,8 +37,18 @@ public class C3P0Test {
 
     @Test
     void connectWithConf() throws SQLException {
-        var dataSource = new ComboPooledDataSource("c3p0App");
-        var cxn = dataSource.getConnection();
+        var cxn = DATA_SOURCE.getConnection();
         System.out.println(cxn);
+    }
+
+    @Test
+    void use() {
+        try (var cxn = DATA_SOURCE.getConnection()) {
+            var dao = new CustomerDAOImpl();
+            var customer = dao.getCustomerById(cxn, 2);
+            System.out.println(customer);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }

810c9da4872ed15c60c543df12ecc57216c64457
胡子豪 <wuqieor@outlook.com>
2020-10-14 01:00:54 +0800

    愚蠢的万能异常处理

--- jdbc/src/apache_dbutils/QueryRunnerTest.java
+++ jdbc/src/apache_dbutils/QueryRunnerTest.java
@@ -9,8 +9,10 @@ import org.apache.commons.dbutils.handlers.MapHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Properties;
+import java.util.Random;
 
 import javax.sql.DataSource;
 
@@ -21,6 +23,120 @@ import pojo.Customer;
  * @since 2020/10/13 22:01
  */
 public class QueryRunnerTest {
+    @FunctionalInterface
+    interface RunnableThrow {
+        void run() throws Exception;
+    }
+
+    @FunctionalInterface
+    interface ExceptionHandler<T extends Exception> {
+        void handle();
+    }
+
+    public static class ExceptionCatcher {
+        public static void run(RunnableThrow runnableThrow, ExceptionHandler<?>... handlers) {
+            try {
+                runnableThrow.run();
+            } catch (Exception e) {
+                if (0 == handlers.length) e.printStackTrace();
+                // 根据 抛出异常对象 的类型进行相应处理
+                for (var handler : handlers) {
+                    if (e.getClass().equals(getActualTypeArgument(handler))) handler.handle();
+                }
+            }
+        }
+
+        private static Type getActualTypeArgument(ExceptionHandler<?> handler) {
+            var genericInterface =
+                    (ParameterizedType) handler.getClass().getGenericInterfaces()[0];
+            return genericInterface.getActualTypeArguments()[0];
+        }
+
+        @Test
+        void testRun() {
+            var handler = new ExceptionHandler<>() {
+                @Override
+                public void handle() {
+                    System.out.println("exception");
+                }
+            };
+
+            var aioobeHandler = new ExceptionHandler<ArrayIndexOutOfBoundsException>() {
+
+                @Override
+                public void handle() {
+                    System.out.println("array index out of bounds");
+                }
+            };
+
+            var npeHandler = new ExceptionHandler<NullPointerException>() {
+
+                @Override
+                public void handle() {
+                    System.out.println("null");
+                }
+            };
+
+            ExceptionCatcher.run(() -> {
+                switch (new Random().nextInt(3)) {
+                    case 0 -> throw new NullPointerException();
+                    case 1 -> throw new ArrayIndexOutOfBoundsException();
+                    default -> throw new Exception();
+                }
+            }, handler, aioobeHandler, npeHandler);
+
+            try {
+                switch (new Random().nextInt(3)) {
+                    case 0 -> throw new NullPointerException();
+                    case 1 -> throw new ArrayIndexOutOfBoundsException();
+                    default -> throw new Exception();
+                }
+            } catch (NullPointerException e) {
+                System.out.println("exception");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("array index out of bounds");
+            } catch (Exception e) {
+                System.out.println("null");
+            }
+        }
+
+        @Test
+        void testLessLambda() {
+            RunnableThrow runnableThrow = () -> {
+                switch (new Random().nextInt(2)) {
+                    case 0 -> throw new NullPointerException();
+                    case 1 -> throw new ArrayIndexOutOfBoundsException();
+                }
+            };
+
+            ExceptionHandler<ArrayIndexOutOfBoundsException>
+                    nullPointerExceptionExceptionHandler = () -> System.out.println("hello");
+
+            var handler = new ExceptionHandler<NullPointerException>() {
+                @Override
+                public void handle() {
+                    System.out.println("null");
+                }
+            };
+
+            ExceptionCatcher.run(runnableThrow, nullPointerExceptionExceptionHandler, handler);
+        }
+
+        @Test
+        void testMoreLambda() {
+            ExceptionCatcher.run(() -> {
+                switch (new Random().nextInt(2)) {
+                    case 0 -> throw new NullPointerException();
+                    case 1 -> throw new ArrayIndexOutOfBoundsException();
+                }
+            }, (ExceptionHandler<ArrayIndexOutOfBoundsException>) () -> {
+                System.out.println("hello");
+            }, (ExceptionHandler<NullPointerException>) () -> {
+                System.out.println("null");
+            });
+        }
+    }
+
     private static QueryRunner queryRunner;
 
     static {
@@ -37,7 +153,7 @@ public class QueryRunnerTest {
 
     @Test
     void insert() {
-        try {
+        ExceptionCatcher.run(() -> {
             var sql = """
                     insert into customers(name, email, birth)
                     values(?, ?, ?)
@@ -45,15 +161,13 @@ public class QueryRunnerTest {
             var affectedRowNumber = queryRunner.update(sql,
                     "李容蓉", "lrr@lrr.com", "1888-01-01");
             System.out.println(affectedRowNumber);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        });
     }
 
     // BeanHandler是ResultSetHandler的实现类，用于封装表中的一条记录。
     @Test
     void queryCustomer() {
-        try {
+        ExceptionCatcher.run(() -> {
             var sql = """
                     select id, name, email, birth
                     from customers
@@ -62,14 +176,12 @@ public class QueryRunnerTest {
             var beanHandler = new BeanHandler<>(Customer.class);
             var customer = queryRunner.query(sql, beanHandler, 22);
             System.out.println(customer);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        });
     }
 
     @Test
     void queryCustomerList() {
-        try {
+        ExceptionCatcher.run(() -> {
             var sql = """
                     select id, name, email, birth
                     from customers
@@ -78,14 +190,12 @@ public class QueryRunnerTest {
             var beanListHandler = new BeanListHandler<>(Customer.class);
             var customerList = queryRunner.query(sql, beanListHandler, 22);
             customerList.forEach(System.out::println);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        });
     }
 
     @Test
     void queryMap() {
-        try {
+        ExceptionCatcher.run(() -> {
             var sql = """
                     select id, name, email, birth
                     from customers
@@ -94,14 +204,12 @@ public class QueryRunnerTest {
             var mapHandler = new MapHandler();
             var map = queryRunner.query(sql, mapHandler, 22);
             System.out.println(map);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        });
     }
 
     @Test
     void queryMapList() {
-        try {
+        ExceptionCatcher.run(() -> {
             var sql = """
                     select id, name, email, birth
                     from customers
@@ -110,8 +218,6 @@ public class QueryRunnerTest {
             var mapListHandler = new MapListHandler();
             var mapList = queryRunner.query(sql, mapListHandler, 22);
             System.out.println(mapList);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        });
     }
 }

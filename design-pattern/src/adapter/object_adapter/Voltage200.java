d6d92000ef145b9c9662a2c2834e8496cf44f690
胡子豪 <wuqieor@outlook.com>
2020-11-02 16:40:14 +0800

    设计模式-适配器模式：对象适配器模式

---
+++ design-pattern/src/adapter/object_adapter/Voltage200.java
@@ -0,0 +1,13 @@
+package adapter.object_adapter;
+
+/**
+ * @author huzihao
+ * @since 2020/11/2 16:27
+ */
+public class Voltage200 {
+    public int output200V() {
+        int vol = 200;
+        System.out.println("输出" + vol + "电压");
+        return vol;
+    }
+}

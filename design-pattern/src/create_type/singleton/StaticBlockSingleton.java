14193ca430eefab028f8714e432955f7152d7369
胡子豪 <wuqieor@outlook.com>
2020-11-05 16:13:09 +0800

    单例模式：静态代码块初始化

---
+++ design-pattern/src/create_type/singleton/StaticBlockSingleton.java
@@ -0,0 +1,24 @@
+package create_type.singleton;
+
+/**
+ * @author huzihao
+ * @since 2020/11/5 10:51
+ */
+public class StaticBlockSingleton {
+    private static StaticBlockSingleton instance;
+
+    private StaticBlockSingleton() {
+    }
+
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+}

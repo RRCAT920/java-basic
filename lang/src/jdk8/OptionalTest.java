860fea508690936b8e8e4ed38ebac4d1629181ab
胡子豪 <wuqieor@outlook.com>
2020-11-05 13:52:45 +0800

    Optional和其他判空操作的比较

--- lang/src/jdk8/OptionalTest.java
+++ lang/src/jdk8/OptionalTest.java
@@ -1,5 +1,6 @@
 package jdk8;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -13,17 +14,37 @@ import jdk8.data.Son;
  */
 public class OptionalTest {
     @Test
-    public void testSon() {
+    public void testGetSonName() {
         System.out.println(getSonName(null));
         System.out.println(getSonName(new Father()));
         System.out.println(getSonName(new Father("舒克", new Son("贝塔"))));
     }
 
-    private String getSonName(Father father) {
-        var fatherOpt = Optional.ofNullable(father)
-                .orElse(new Father("小头爸爸", new Son("大头儿子")));
-        return Optional.ofNullable(fatherOpt.getSon())
-                .orElse(new Son("黑猫警长"))
-                .getName();
+    private static String getSonName(Father father) {
+        return Optional.of(father)
+                .map(Father::getSon)
+                .orElseThrow().getName();
+    }
+
+    @Test
+    public void testGetSonNameWithAssert() {
+        System.out.println(getSonNameWithAssert(new Father()));
+    }
+
+    private String getSonNameWithAssert(Father father) {
+        assert null != father;
+        var son = father.getSon();
+        assert null != son;
+        return son.getName();
+    }
+
+    @Test
+    public void testGetSonNameWithAnnotation() {
+        System.out.println(getSonNameWithAnnotation(new Father()));
+    }
+
+    private String getSonNameWithAnnotation(@NotNull Father father) {
+        @NotNull var son = father.getSon();
+        return son.getName();
     }
 }

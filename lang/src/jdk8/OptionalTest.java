125a8a1a2ce059b45f0ecdd36876663465a74ea8
胡子豪 <wuqieor@outlook.com>
2020-11-05 16:05:35 +0800

    Optional在Jdk8和Jdk9时的常用方法

--- lang/src/jdk8/OptionalTest.java
+++ lang/src/jdk8/OptionalTest.java
@@ -14,6 +14,49 @@ import jdk8.data.Son;
  */
 public class OptionalTest {
     @Test
+    public void optionalBasicExample() {
+        System.out.println(division(1, 0));
+        System.out.println(division(2, 2));
+    }
+
+    public static Optional<Integer> division(Integer i1, Integer i2) {
+        if (0 == i2) return Optional.empty();
+        return Optional.of(i1 / i2);
+    }
+
+    @Test
+    public void jdk8OptionalMethods() {
+        // jdk8
+        var opt1 = division(2, 1);
+        opt1.ifPresent(System.out::println);
+
+        var opt2 = division(2, 0);
+        opt2.ifPresent(System.out::println);
+
+        System.out.println("Opt2没有值的情况下，默认值为" + opt2.orElse(0));
+
+        if (opt2.isPresent()) {
+            System.out.println("Opt2有值");
+        } else {
+            System.out.println("Opt2没有值");
+        }
+    }
+
+    @Test
+    public void jdk9OptionalMethods() {
+        var opt1 = division(2, 1);
+        var opt2 = division(2, 0);
+
+        // ifPresentOrElse
+        opt1.ifPresentOrElse(System.out::println, () -> System.out.println("没有值"));
+
+        opt2.ifPresentOrElse(System.out::println, () -> System.out.println("没有值"));
+
+        // or
+        System.out.println(opt2.or(() -> Optional.of(0)));
+    }
+
+    @Test
     public void testGetSonName() {
         System.out.println(getSonName(null));
         System.out.println(getSonName(new Father()));
